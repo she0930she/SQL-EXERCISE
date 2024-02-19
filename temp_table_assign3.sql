2ca8bf0d91f7085d9b2e86a75364d8ac1435e693
she0930she <she0930she@hotmail.com>
2024-02-18 23:52:45 -0800

    subquery practice Q1-4

--- temp_table_assign3.sql
+++ temp_table_assign3.sql
@@ -2,23 +2,85 @@
 --All scenarios are based on Database NORTHWIND.
 
 --1.      List all cities that have both Employees and Customers.
+SELECT DISTINCT(e.City)
+FROM dbo.Employees e
+JOIN dbo.Customers c ON e.City = c.City
+
+
+
+--tester
+SELECT *
+FROM dbo.Employees
+
+SELECT *
+FROM dbo.Customers
 
 
 --2.      List all cities that have Customers but no Employee.
 
 --a.      Use sub-query
+SELECT c.City
+FROM dbo.Customers c
+WHERE c.City NOT IN (
+    SELECT e.City
+    FROM dbo.Employees e
+) 
+
+--tester
+SELECT *
+FROM dbo.Employees
+
 
 --b.      Do not use sub-query
+SELECT DISTINCT(c.City)
+FROM dbo.Customers c
+LEFT JOIN dbo.Employees e ON e.City = c.City
+WHERE e.City IS NULL -- no right table at all
+
+
+
 
 --3.      List all products and their total order quantities throughout all orders.
+SELECT ProductID, SUM(Quantity) tolQuantity
+FROM dbo.[Order Details] od
+GROUP BY ProductID
+
+
+
 
 --4.      List all Customer Cities and total products ordered by that city.
+SELECT c.City, SUM(Quantity) tolProductsOrdered
+FROM dbo.Customers c
+JOIN dbo.Orders o On o.CustomerID = c.CustomerID
+JOIn dbo.[Order Details] od ON od.OrderID = o.OrderID
+GROUP BY c.City
+
+--tester
+SELECT *
+FROM dbo.Orders
+
+SELECT
+    c.City,
+    SUM(od.Quantity) AS TotalProductsOrdered
+FROM
+    Customers c
+JOIN
+    Orders o ON c.CustomerID = o.CustomerID
+JOIN
+    [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY
+    c.City;
+
 
 --5.      List all Customer Cities that have at least two customers.
 
 --a.      Use union
 
 --b.      Use sub-query and no union
+
+
+
+
 
 --6.      List all Customer Cities that have ordered at least two different kinds of products.
 
@@ -35,3 +97,4 @@
 --10.  List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, and also the city of most total quantity of products ordered from. (tip: join  sub-query)
 
 --11. How do you remove the duplicates record of a table?
+
