9749b8c1f177d823b5d303fa1746f68bbee6b1fe
she0930she <she0930she@hotmail.com>
2024-02-18 18:05:32 -0800

    assign3 upload

---
+++ temp_table_assign3.sql
@@ -0,0 +1,37 @@
+--Write queries for following scenarios
+--All scenarios are based on Database NORTHWIND.
+
+--1.      List all cities that have both Employees and Customers.
+
+
+--2.      List all cities that have Customers but no Employee.
+
+--a.      Use sub-query
+
+--b.      Do not use sub-query
+
+--3.      List all products and their total order quantities throughout all orders.
+
+--4.      List all Customer Cities and total products ordered by that city.
+
+--5.      List all Customer Cities that have at least two customers.
+
+--a.      Use union
+
+--b.      Use sub-query and no union
+
+--6.      List all Customer Cities that have ordered at least two different kinds of products.
+
+--7.      List all Customers who have ordered products, but have the ‘ship city’ on the order different from their own customer cities.
+
+--8.      List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
+
+--9.      List all cities that have never ordered something but we have employees there.
+
+--a.      Use sub-query
+
+--b.      Do not use sub-query
+
+--10.  List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, and also the city of most total quantity of products ordered from. (tip: join  sub-query)
+
+--11. How do you remove the duplicates record of a table?
